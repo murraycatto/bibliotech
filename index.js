5559e8aed34050400212cabd9f1bcf42031f5cde
Murray Catto <murraycatto@live.co.za>
2018-02-25 14:59:50 +0200

    restarted from scratch

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ const db = require('./config/db');
 const app = express();
 const port = 8000;
 app.use(bodyParser.urlencoded({extended:true}));
-app.use(express.static('./app/views'));
+app.use(express.static('./service-worker.js'));
+app.use(express.static('./images'));
+app.use(express.static('./src'));
 app.use(express.static('./bower_components'));
 
 // DB
